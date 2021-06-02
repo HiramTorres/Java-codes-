8e06c371559b5e56213199b68ff53bd01c84c2eb
Hiram Torres <hiram.ht10@gmail.com>
2021-06-02 16:22:00 -0500

    Agregué codigo

---
+++ Reto.java
@@ -0,0 +1,19 @@
+public class Reto{
+    public static String suma(int valor1, int valor2) {
+
+        int resultado = valor1 + valor2;
+        String salida = ("El resultado es: " + resultado); 
+        
+        return salida;
+    }
+ 
+ 
+    public static void main(String[]args) {
+ 
+        Reto reto = new Reto();
+        String resultado = reto.suma(10,5);
+ 
+        System.out.println(resultado);
+    }
+ 
+}
